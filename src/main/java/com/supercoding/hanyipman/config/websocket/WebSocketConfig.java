25fdcfaf1371c213404fa486ddee36a86c475f05
no.oneho <99154108+no-oneho@users.noreply.github.com>
2023-09-22 08:46:59 +0900

    Update WebSocketConfig.java

--- src/main/java/com/supercoding/hanyipman/config/websocket/WebSocketConfig.java
+++ src/main/java/com/supercoding/hanyipman/config/websocket/WebSocketConfig.java
@@ -11,7 +11,7 @@ public class WebSocketConfig  {
     public SocketIOServer socketIOServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
 
-        config.setHostname("ec2-54-180-103-214.ap-northeast-2.compute.amazonaws.com");
+        config.setHostname("0.0.0.0");
 
         config.setPort(8088); // 포트를 필요에 따라 수정
 
